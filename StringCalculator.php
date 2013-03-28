fc6ed74093f478ff0347c30d5c733aa8e977c925
Steven Klar <steven.klar@mayflower.de>
2013-03-28 17:16:24 +0100

    Initial commit.

---
+++ StringCalculator.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Class StringCalculator
+ */
+class StringCalculator
+{
+    /**
+     * @var string
+     */
+    private $delimiter = ';';
+
+    /**
+     * @var string
+     */
+    private $customDelimiter = "//;\n";
+
+    /**
+     * @var array
+     */
+    private $negativeNumbers = array();
+
+    /**
+     * @param $numbers
+     *
+     * @return int
+     */
+    public function add($numbers)
+    {
+        if (is_string($numbers)) {
+            return $this->addString($numbers);
+        }
+
+        return $numbers;
+    }
+
+    /**
+     * @param $stringNumbers
+     *
+     * @return int
+     */
+    private function addString($stringNumbers)
+    {
+        if ($this->hasCustomDelimiter($stringNumbers)) {
+            $delimitedStringNumbers = str_replace($this->customDelimiter, '', $stringNumbers);
+
+            $numbers = $this->filterNumbers($delimitedStringNumbers, $this->delimiter);
+        } else {
+            // has no custom delimiter
+            $numbers = $this->filterNumbers($stringNumbers);
+        }
+        $result = $this->calculateNumbers($numbers);
+
+        return $result;
+    }
+
+    /**
+     * @param $stringNumbers
+     *
+     * @return bool
+     */
+    private function hasCustomDelimiter($stringNumbers)
+    {
+        return !strncmp($stringNumbers, $this->customDelimiter, strlen($this->customDelimiter));
+    }
+
+    /**
+     * @param $numbers
+     *
+     * @return int
+     * @throws Exception
+     */
+    private function calculateNumbers($numbers)
+    {
+        $result = 0;
+
+        foreach ($numbers as $number) {
+            if ($this->isValidNumber($number)) {
+                $result += $number;
+            }
+        }
+
+        if ($this->hasNegativeNumbers()) {
+            $errorMessage = sprintf('negatives not allowed (%s)', implode(', ', $this->negativeNumbers));
+            throw new Exception($errorMessage);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $number
+     *
+     * @return boo
+     */
+    private function isValidNumber($number)
+    {
+        if ($number < 0) {
+            array_push($this->negativeNumbers, $number);
+        }
+
+        if ($number > 1000) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $stringNumbers
+     * @param string $delimiter
+     *
+     * @return array
+     */
+    private function filterNumbers($stringNumbers, $delimiter = ',')
+    {
+        $numbers = array();
+
+        $explodedNumbersWithComma = explode($delimiter, $stringNumbers);
+
+        foreach ($explodedNumbersWithComma as $number) {
+            $explodedNumberWithNewline = explode("\n", $number);
+
+            if (is_array($explodedNumberWithNewline)) {
+                foreach ($explodedNumberWithNewline as $newlineNumber) {
+                    array_push($numbers, $newlineNumber);
+                }
+            } else {
+                array_push($numbers, $number);
+            }
+        }
+
+        return $numbers;
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasNegativeNumbers()
+    {
+        return !empty($this->negativeNumbers);
+    }
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ *
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Z+
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *            +$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *                                     ~7$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *                                    I$$$$$$$$$$$$$$$$$$$$$$$$$$$$+
+ *                                  ,I$$$$$$$$$$$$$$$Z7$$$$$$$$$$$$+
+ *                                 ,IZ$$$$$$$$$$$$$$$$?7$$$$$$$$$$$+
+ *                               ,I$$$$$$$$$$$$$$$$$+, 7$$$$$$$$$$$+
+ *                              ,$$$$$$$$$$$$$$$$7$?  ,7$$$$$$$$$$$+
+ *                            ,I$$$$$Z$$$$$$$$$$$?    ,7$$Z$$$$$$$$+
+ *                           I$$$$$$$$$$$$$$$$$$,     ,7$$$$$$$$$$$+
+ *                         ,I$$$$$$$$$$$$$$$$$+,       7$$$$$$$$$$$+
+ *                       ,I$$$$$$$$$$$$$$$ZZ?          7$$$$$$$$$$$+
+ *                      I$$$$$Z$$$$$$$$$$$$            7$$$$$$$$$$$+
+ *                    ,I$$$$$$$$$$$$$$$$$+,            7$$$$$$$$$$$+
+ *                  ,IZ$$$$$$$$$$$$$$$$+,              7$$$$$$$$$$$+
+ *                 ,IZ$$$$$$$$$$$$$$$$?,               7$$$$$$$$$$$+
+ *                 $$$$$$$$$$$$$$$$$$+                 7$$$$$$$$$$$+
+ *              ,I$$$$$$$$$$$$$$$$$+, ,~+?II+=:        7$$$$$$$$$$$+
+ *             IZ$$$$$$$$$$$$$$$$?, ,=$$$$$$$$$$:      7$$$$$$$$$$$+
+ *           ,?$$$$$$$$$$$$$$$$$+  ~7$$$$$$$$$$$Z?,    7$$$$$$$$$$$+
+ *         ,I$$$$$$$$$$$$$$$$$+,  =$$$$$$$$$$$$$$$I,   7$$$$$$$$$$$+
+ *        I$$$$$$$$$$$$$$$$$Z    ,I$$$$$$$$$$$$$$$$=   7$$$$$$$$$$$+
+ *        7$Z$$$$$$$$$$$$$$+,    :7$Z$$$$$$$$$$$$$$?   7$$$$$$$$$$$+
+ *         :$$$$Z$$$$$$$$?,      ,I$$$$$$$$$$$$$$$$=   7$$$$$$$$$$$+
+ *           ~7$$$$$$$$$          ~$$$$$$$$$$$$$$$?,   7$$$$$$$$$$$+
+ *            ~$$$$$$$$?          ,?$$$$$$$$$$$$Z$~    7$$$$$$$$$$$+
+ *             ~7Z$$$+,             ~7$$$$$$$$$$I,     7$$$$$$$$$$$+
+ *               :7+,                 +7$$$$$7I~      ,7$$ZZ$$$$$$Z+
+ *
+ *
+ * ------------------------------------------------------------------
+ *              Created for ProSiebenSat.1 Games                    -
+ *              Developer: Mayflower Bambi Team                     -
+ * ------------------------------------------------------------------
+ */
+
+require_once 'StringCalculator.php';
+
+class StringCalculatorTest extends PHPUnit_Framework_TestCase
+{
+    public function test_add_takeZeroAsString_returnZeroAsNumber()
+    {
+        $this->expectAddResult("", 0);
+    }
+
+    public function test_add_oneNumber_getNumberBack()
+    {
+        $this->expectAddResult("1", 1);
+    }
+
+    public function test_add_twoNumbersInString_sumNumbersAndReturnResult()
+    {
+        $this->expectAddResult("1,2", 3);
+    }
+
+    public function test_add_handleUnknownAmountOfNumbers_sumAllTogether()
+    {
+        $this->expectAddResult("1,2,3,4,5,6,7,8,9", 45);
+    }
+
+    public function test_add_handleAlsoNewLinesInsteadOfComma_sumAllTogether()
+    {
+        $this->expectAddResult("1\n2,3", 6);
+    }
+
+    public function test_add_takesDelimiterAtStart_useCustomDelimiterAsFilter()
+    {
+        $this->expectAddResult("//;\n1;2", 3);
+    }
+
+    public function test_add_numbersBiggerThen1000_shouldBeIgnored()
+    {
+        $this->expectAddResult("2,2000", 2);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function test_add_negativeNumber_throwNegativesNotAllowedException()
+    {
+        $this->expectAddResult("-1,-5,5", -1);
+    }
+
+    /**
+     * @return StringCalculator
+     */
+    private function makeCalculator()
+    {
+        return new StringCalculator();
+    }
+
+    /**
+     * @param $numbers
+     * @param $expectedResult
+     */
+    private function expectAddResult($numbers, $expectedResult)
+    {
+        $calculator = $this->makeCalculator();
+
+        $result = $calculator->add($numbers);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+}
